4c58d1ca21a813e1808eed8bd68a19f63f54f1ea
alfongj <alfongj@gmail.com>
2016-05-30 00:43:01 +0200

    Finished chapter iii

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,49 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    val pseudoNumDays = year * 365 + month * 30 + dayOfMonth
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    override fun compareTo(other: MyDate): Int {
+        return pseudoNumDays - other.pseudoNumDays
+    }
 
-enum class TimeInterval {
-    DAY,
-    WEEK,
-    YEAR
+    infix operator fun plus(timeInterval: TimeInterval): MyDate {
+        return addTimeIntervals(timeInterval, timeInterval.quantity)
+    }
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+enum class TimeInterval(var quantity: Int = 1) {
+    DAY(1),
+    WEEK(1),
+    YEAR(1);
+
+    infix operator fun times(i: Int): TimeInterval {
+        quantity = i
+        return this
+    }
+}
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(date: MyDate) =
+            date.pseudoNumDays in start.pseudoNumDays..endInclusive.pseudoNumDays
+
+    operator fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+
+            var currentDate = start
+
+            override fun hasNext(): Boolean {
+                return currentDate <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val oldDate = currentDate
+                currentDate = currentDate.nextDay()
+                return oldDate
+            }
+
+        }
+    }
+}
--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,7 +3,16 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var _numberOfInvocations = 0
+
+    operator fun invoke(): Invokable {
+        _numberOfInvocations++
+        return this
+    }
+
+    fun getNumberOfInvocations() = _numberOfInvocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +23,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
